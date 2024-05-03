1a4c766ec4f6a2f0ac3fc7af6afe900898e6402b
shashank-priyadarshi <shashank9163882019@gmail.com>
2024-05-03 20:14:27 +0530

    feat: add def & examples for slices in go

--- golang/types/types.go
+++ golang/types/types.go
@@ -189,16 +189,54 @@ func arrays() {
 
 	arr1 = [10]string{"Shashank", "Priyadarshi", "Golang", "Rust", "JavaScript", "TypeScript", "C", "C++", "Python", "R"}
 
+	// TODO 1:
 	// Declare two arrays of type int and type string
 	// Fill integer array with strings
 	// Fill string array with integers
 
+	// TODO 2:
 	// Create another array
 	// This array should also contain integers
 	// Try adding negative values into this array
 	// All values in this array will be limited to 0-255
 }
 
-func slices() {}
+func slices() {
+	// Size of an array is part of its type: var arr [10]string: here 10 is the size of the array
+	// Since Go is statically typed, type of variable arr is fixes as [10]string, which means it will always store a maximum 10 strings
+	// Type of variable arr will not change at runtime
+
+	// Slices offer a solution for dynamically sized lists
+	// Sizes of these lists grow based on requirement at runtime
+	// Slices use arrays internally to store data
+	// A slice is golang has three properties: length, capacity, type
+
+	// var s []string // We declare a slice and define its type by not providing the size
+	// If size provided, it becomes an array, and size is fixed for the array
+
+	s2 := make([]string, 0, 10) // Declaration is done by shorthand :=, definition & initialization is done by the make function
+	// Slice: The size specifies the length. The capacity of the slice is
+	// equal to its length. A second integer argument may be provided to
+	// specify a different capacity; it must be no smaller than the
+	// length. For example, make([]int, 0, 10) allocates an underlying array
+	// of size 10 and returns a slice of length 0 and capacity 10 that is
+	// backed by this underlying array.
+
+	// Length of a slice is the number of items the slice currently holds
+	// Capacity is the number of items the slice can hold
+	// The capacity of a slice can change based on requirement
+	s2[11] = "Shashank" // The same case would be invalid for an array because an array of size 10 will have highest index of 10 -1 = 9
+
+	// Internally, a slice stores data in an array
+	// If type of slice is specified, an array of same type is created, using the capacity of the slice
+	// If number of items goes more than the capacity, a new array of larger capacity is created, all items are moved from old array to new array
+
+	// TODO 3:
+	// Declare a slice of type string
+	// Define the length and capacity at declaration
+	// Do not initialize the slice
+	// Then, put an item in the list at index greater than the capacity
+
+}
 
 func maps() {}
